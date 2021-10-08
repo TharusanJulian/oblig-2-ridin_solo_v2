a133b7368c41c45dd9f5371acf0b9c760d8f2910
TharusanJulian <71075148+TharusanJulian@users.noreply.github.com>
2021-10-08 12:38:48 +0200

    Prøver meg på oppgave 3b på nytt denne gangen prøver jeg å lage sublisten og følge instruksene i oppgave teksten. Men testen sier at jeg failer. Jeg tenker å begynne på neste oppgave istedenfor å sitte med denne oppgaven for lenge.

--- src/main/java/no/oslomet/cs/algdat/Oblig2/DobbeltLenketListe.java
+++ src/main/java/no/oslomet/cs/algdat/Oblig2/DobbeltLenketListe.java
@@ -31,11 +31,26 @@ public class DobbeltLenketListe<T> implements Liste<T> {
         }
     }
 
+    private static void fratilKontroll(int antall, int fra, int til) {
+        if (fra < 0)                                  // fra er negativ
+            throw new IndexOutOfBoundsException
+                    ("fra(" + fra + ") er negativ!");
+
+        if (til > antall)                          // til er utenfor tabellen
+            throw new IndexOutOfBoundsException
+                    ("til(" + til + ") > antall(" + antall + ")");
+
+        if (fra > til)                                // fra er større enn til
+            throw new IllegalArgumentException
+                    ("fra(" + fra + ") > til(" + til + ") - illegalt intervall!");
+    }
+
     // instansvariabler
     private Node<T> hode;          // peker til den første i listen
     private Node<T> hale;          // peker til den siste i listen
     private int antall;            // antall noder i listen
     private int endringer;         // antall endringer i listen
+
 
     public DobbeltLenketListe() {
         hode = hale = null;
@@ -44,18 +59,17 @@ public class DobbeltLenketListe<T> implements Liste<T> {
     }
 
     public DobbeltLenketListe(T[] a) {
-        this();
 
-        if (a == null){
+        if (a == null) {
             //  tom liste
             Objects.requireNonNull("Ikke tilatt null verdier");
         }
         //setter hode og hale sin verdi = null
         hode = hale = new Node<>(null);
 
-        for (int i =0; i < a.length; i++){  // resten av verdiene
+        for (int i = 0; i < a.length; i++) {  // resten av verdiene
 
-            if(a[i] != null) {
+            if (a[i] != null) {
                 hode = new Node<>(a[i]);
                 antall++;
             }
@@ -63,8 +77,23 @@ public class DobbeltLenketListe<T> implements Liste<T> {
     }
 
     public Liste<T> subliste(int fra, int til) {
-        throw new UnsupportedOperationException();
+        fratilKontroll(fra, til, antall);
+        DobbeltLenketListe<T> liste = new DobbeltLenketListe<>();
+        int lengde = til - fra;
+        if (lengde > 0) {
+            for (int i = fra; i < til; i++) {
+                Node<T> temp = finnNode(fra);
+                liste.leggInn(temp.verdi);
+                fra++;
+            }
+        }
+        endringer = 0;
+        return liste;
     }
+
+
+
+
     private Node<T> finnNode(int indeks)
     {
         Node<T> p;
@@ -100,7 +129,10 @@ public class DobbeltLenketListe<T> implements Liste<T> {
 
     @Override
     public boolean leggInn(T verdi) {
-        Objects.requireNonNull(verdi,"Null verdi");
+        if (verdi== null) {
+            //  tom liste
+            Objects.requireNonNull("Ikke tilatt null verdier");
+        }
 
         if (antall == 0) {
             hode = hale = new Node<>(verdi, null, null);
@@ -248,43 +280,50 @@ public class DobbeltLenketListe<T> implements Liste<T> {
         throw new UnsupportedOperationException();
     }
 
-    private class DobbeltLenketListeIterator implements Iterator<T> {
+
+    private class DobbeltLenketListeIterator implements Iterator<T>
+    {
         private Node<T> denne;
         private boolean fjernOK;
         private int iteratorendringer;
 
-        private DobbeltLenketListeIterator() {
-            denne = hode;     // p starter på den første i listen
+        private DobbeltLenketListeIterator()
+        {
+            denne = hode;     // denne starter på den første i listen
             fjernOK = false;  // blir sann når next() kalles
             iteratorendringer = endringer;  // teller endringer
         }
 
-        private DobbeltLenketListeIterator(int indeks) {
-            throw new UnsupportedOperationException();
+        private DobbeltLenketListeIterator(int indeks)
+        {
+            throw new UnsupportedOperationException("Ikke laget ennå!");
         }
 
         @Override
-        public boolean hasNext() {
-            return denne != null;
+        public boolean hasNext()
+        {
+            return denne != null;  // denne koden skal ikke endres!
         }
 
         @Override
-        public T next() {
-            throw new UnsupportedOperationException();
+        public T next()
+        {
+            throw new UnsupportedOperationException("Ikke laget ennå!");
         }
 
         @Override
-        public void remove() {
-            throw new UnsupportedOperationException();
+        public void remove()
+        {
+            throw new UnsupportedOperationException("Ikke laget ennå!");
         }
 
-    } // class DobbeltLenketListeIterator
+    } // DobbeltLenketListeIterator
 
-    public static <T> void sorter(Liste<T> liste, Comparator<? super T> c) {
-        throw new UnsupportedOperationException();
+    public static <T> void sorter(Liste<T> liste, Comparator<? super T> c)
+    {
+        throw new UnsupportedOperationException("Ikke laget ennå!");
     }
-
-} // class DobbeltLenketListe
+}
 
 
 
